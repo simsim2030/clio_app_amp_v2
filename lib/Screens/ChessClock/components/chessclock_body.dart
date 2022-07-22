593fa324d44c4ee5b5014aaef64668bfbd0525ca
simsim2030 <simeoncheuk@gmail.com>
2022-07-22 14:31:45 +1000

    added chessclock page

--- lib/Screens/ChessClock/components/chessclock_body.dart
+++ lib/Screens/ChessClock/components/chessclock_body.dart
@@ -1,17 +1,78 @@
 import 'package:flutter/material.dart';
+import 'chessclocktimer.dart';
+import 'chessclockfunction.dart';
+import 'dart:async';
 
 class ChessClockBody extends StatefulWidget {
-  const ChessClockBody({
-    Key? key,
-  }) : super(key: key);
-
   @override
   State<ChessClockBody> createState() => _ChessClockBodyState();
 }
 
 class _ChessClockBodyState extends State<ChessClockBody> {
+  ChessClock _topClock = ChessClock(
+    getNowMillis: () => DateTime.now().millisecondsSinceEpoch,
+    timeControlMillis: 5 * 60 * 1000,
+    incrementsMillis: 1000,
+  );
+
+  ChessClock _bottomClock = ChessClock(
+    getNowMillis: () => DateTime.now().millisecondsSinceEpoch,
+    timeControlMillis: 5 * 60 * 1000,
+    incrementsMillis: 1000,
+  );
+
+  late Timer _timer;
+
+  @override
+  void initState() {
+    super.initState();
+    _timer = Timer.periodic(Duration(milliseconds: 200), (timer) {
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _timer.cancel();
+  }
+
+  void _onTopPressed() {
+    _topClock.pause();
+    _bottomClock.start();
+  }
+
+  void _onBottomPressed() {
+    _topClock.start();
+    _bottomClock.pause();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            child: ChessClockTimer(
+              onPressed: _onTopPressed,
+              isReversed: true,
+              isTicking: _topClock.isTicking(),
+              isTimeup: _topClock.isTimeUp(),
+              availableTime:
+                  Duration(milliseconds: _topClock.getAvailableMillis()),
+            ),
+          ),
+          Expanded(
+            child: ChessClockTimer(
+              onPressed: _onBottomPressed,
+              isTicking: _bottomClock.isTicking(),
+              isTimeup: _bottomClock.isTimeUp(),
+              availableTime:
+                  Duration(milliseconds: _bottomClock.getAvailableMillis()),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
---
+++ lib/Screens/ChessClock/components/chessclocktimer.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class ChessClockTimer extends StatelessWidget {
+  final bool isReversed;
+  final bool isTicking;
+  final bool isTimeup;
+  final VoidCallback onPressed;
+
+  final Duration availableTime;
+
+  const ChessClockTimer({
+    Key? key,
+    this.isReversed = false,
+    this.isTicking = false,
+    this.isTimeup = false,
+    required this.onPressed,
+    required this.availableTime,
+  }) : super(key: key);
+
+  Color _getColor() {
+    if (isTicking) {
+      return Colors.orange;
+    }
+
+    if (isTimeup) {
+      return Colors.red;
+    }
+
+    return Colors.black.withAlpha(30);
+  }
+
+  String _getAvailableTimeText() {
+    return availableTime.toString().substring(2, 7);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onPressed,
+      child: Container(
+        color: _getColor(),
+        child: Center(
+          child: RotatedBox(
+            quarterTurns: isReversed ? 2 : 0,
+            child: Text(
+              _getAvailableTimeText(),
+              style: TextStyle(
+                fontSize: 70,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
