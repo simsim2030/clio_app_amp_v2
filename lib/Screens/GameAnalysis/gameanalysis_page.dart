cfbf65b5ae750d599fc5645127cb86b011b8d2ed
simsim2030 <simeoncheuk@gmail.com>
2022-10-21 12:50:11 +1100

    storage connected to s3

--- lib/Screens/GameAnalysis/gameanalysis_page.dart
+++ lib/Screens/GameAnalysis/gameanalysis_page.dart
@@ -15,139 +15,141 @@ class GameAnalysis extends ConsumerWidget {
 
   GameAnalysis({Key? key}) : super(key: key);
 
-  // Future<bool> uploadImage({
-  //   required BuildContext context,
-  //   required WidgetRef ref,
-  // }) async {
-  //   final picker = ImagePicker();
-  //   final pickedFile = await picker.pickImage(source: ImageSource.gallery);
-  //   if (pickedFile == null) {
-  //     return false;
-  //   }
+  Future<bool> uploadImage({
+    required BuildContext context,
+    required WidgetRef ref,
+  }) async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+    if (pickedFile == null) {
+      return false;
+    }
 
-  //   final file = File(pickedFile.path);
-  //   showDialog<String>(
-  //       context: context,
-  //       barrierDismissible: false,
-  //       builder: (BuildContext context) {
-  //         return const UploadProgressDialog();
-  //       });
-  //   await ref.read(storageFilesControllerProvider).uploadFile(file);
-  //   return true;
-  // }
+    final file = File(pickedFile.path);
+    showDialog<String>(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext context) {
+          return const UploadProgressDialog();
+        });
+    await ref.read(storageFilesControllerProvider).uploadFile(file);
+    return true;
+  }
 
-  // Future<void> deletestorageFile(
-  //     BuildContext context, WidgetRef ref, StorageFile storageFile) async {
-  //   var value = await showDialog<bool>(
-  //       context: context,
-  //       builder: (BuildContext context) {
-  //         return const DeleteStorageFileDialog();
-  //       });
-  //   value ??= false;
+  Future<void> deletestorageFile(
+      BuildContext context, WidgetRef ref, StorageFile storageFile) async {
+    var value = await showDialog<bool>(
+        context: context,
+        builder: (BuildContext context) {
+          return const DeleteStorageFileDialog();
+        });
+    value ??= false;
 
-  //   if (value) {
-  //     await ref
-  //         .read(storageFilesControllerProvider)
-  //         .deleteFile(storageFile.key);
-  //     ref.refresh(storageFilesListFutureProvider);
-  //   }
-  // }
+    if (value) {
+      await ref
+          .read(storageFilesControllerProvider)
+          .deleteFile(storageFile.key);
+      ref.refresh(storageFilesListFutureProvider);
+    }
+  }
 
   final helloWorldProvider = Provider((_) => 'Hello world');
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    // final storageItems = ref.watch(storageFilesListFutureProvider);
-    final value = ref.watch(helloWorldProvider);
+    final storageItems = ref.watch(storageFilesListFutureProvider);
+    // final value = ref.watch(helloWorldProvider);
     // return Text(value); // Hello world
     return Scaffold(
-      appBar: null,
-      body: Center(),
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: Text('Live Chess'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          uploadImage(
+            context: context,
+            ref: ref,
+          ).then((value) {
+            if (value) {
+              ref.refresh(storageFilesListFutureProvider);
+
+              Navigator.of(context, rootNavigator: true).pop();
+            }
+          });
+        },
+        child: const Icon(Icons.add),
+      ),
+
+      body: Container(
+        padding: EdgeInsets.all(8.0),
+        child: storageItems.when(
+          data: (items) => items.isEmpty
+              ? const Center(
+                  child: Text('No Items'),
+                )
+              : Column(
+                  children: [
+                    Expanded(
+                      child: GridView.builder(
+                        key: const Key('StorageItemsGridView'),
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 1,
+                          mainAxisSpacing: 5,
+                          crossAxisSpacing: 5,
+                          childAspectRatio: 5,
+                        ),
+                        itemCount: items.length,
+                        itemBuilder: (context, index) => InkWell(
+                          highlightColor: Colors.red.withOpacity(0.4),
+                          splashColor: Colors.amber.withOpacity(0.5),
+                          onLongPress: (() {
+                            deletestorageFile(context, ref, items[index]);
+                          }),
+                          child: StorageFileTile(
+                            storageFile: items[index],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+          error: (e, st) => const Center(
+            child: Text('Error'),
+          ),
+          loading: () => const Center(
+            child: CircularProgressIndicator(),
+          ),
+        ),
+      ),
+      // body: Column(
+      //   children: [
+      //     Expanded(
+      //       child: ListView(
+      //         children: [
+      //           ListTile(
+      //             title: Text('Game 1 - 20/09/2015'),
+      //             subtitle: Text('Sim - Calvin'),
+      //             leading: Icon(Icons.label),
+      //           ),
+      //         ],
+      //       ),
+      //     ),
+      //     ElevatedButton(
+      //       onPressed: () {
+      //         storageservice.createAndUploadFile();
+      //       },
+      //       child: Text('update test'),
+      //     ),
+      //     ElevatedButton(
+      //       onPressed: () {
+      //         storageservice.refresh(1);
+      //       },
+      //       child: Text('refresh'),
+      //     )
+      //   ],
+      // ),
     );
-    //   appBar: AppBar(
-    //     title: Text('Live Chess'),
-    //   ),
-    //   floatingActionButton: FloatingActionButton(
-    //     onPressed: () {
-    //       uploadImage(
-    //         context: context,
-    //         ref: ref,
-    //       ).then((value) {
-    //         if (value) {
-    //           ref.refresh(storageFilesListFutureProvider);
-
-    //           Navigator.of(context, rootNavigator: true).pop();
-    //         }
-    //       });
-    //     },
-    //     child: const Icon(Icons.add),
-    //   ),
-
-    // body: storageItems.when(
-    //   data: (items) => items.isEmpty
-    //       ? const Center(
-    //           child: Text('No Items'),
-    //         )
-    //       : Column(
-    //           children: [
-    //             Expanded(
-    //               child: GridView.builder(
-    //                 key: const Key('StorageItemsGridView'),
-    //                 gridDelegate:
-    //                     const SliverGridDelegateWithFixedCrossAxisCount(
-    //                   crossAxisCount: 2,
-    //                   mainAxisSpacing: 10,
-    //                   crossAxisSpacing: 5,
-    //                 ),
-    //                 itemCount: items.length,
-    //                 itemBuilder: (context, index) => InkWell(
-    //                   highlightColor: Colors.red.withOpacity(0.4),
-    //                   splashColor: Colors.amber.withOpacity(0.5),
-    //                   onLongPress: (() {
-    //                     deletestorageFile(context, ref, items[index]);
-    //                   }),
-    //                   child: StorageFileTile(
-    //                     storageFile: items[index],
-    //                   ),
-    //                 ),
-    //               ),
-    //             ),
-    //           ],
-    //         ),
-    //   error: (e, st) => const Center(
-    //     child: Text('Error'),
-    //   ),
-    //   loading: () => const Center(
-    //     child: CircularProgressIndicator(),
-    //   ),
-    // ),
-    // body: Column(
-    //   children: [
-    //     Expanded(
-    //       child: ListView(
-    //         children: [
-    //           ListTile(
-    //             title: Text('Game 1 - 20/09/2015'),
-    //             subtitle: Text('Sim - Calvin'),
-    //             leading: Icon(Icons.label),
-    //           ),
-    //         ],
-    //       ),
-    //     ),
-    //     ElevatedButton(
-    //       onPressed: () {
-    //         storageservice.createAndUploadFile();
-    //       },
-    //       child: Text('update test'),
-    //     ),
-    //     ElevatedButton(
-    //       onPressed: () {
-    //         storageservice.refresh(1);
-    //       },
-    //       child: Text('refresh'),
-    //     )
-    //   ],
-    // ),
-    // );
   }
 }
--- lib/Screens/GameAnalysis/storage_feature/ui/storage_file_tile.dart
+++ lib/Screens/GameAnalysis/storage_feature/ui/storage_file_tile.dart
@@ -12,14 +12,38 @@ class StorageFileTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 4.0,
-      child: CachedNetworkImage(
-        cacheKey: storageFile.key,
-        imageUrl: storageFile.url,
-        height: double.infinity,
-        width: double.infinity,
-        fit: BoxFit.cover,
+    return Container(
+      height: 50,
+      child: Card(
+        elevation: 2.0,
+        clipBehavior: Clip.antiAlias,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
+        color: Colors.white,
+        // child: CachedNetworkImage(
+        //   cacheKey: storageFile.key,
+        // imageUrl: storageFile.url,
+        // height: double.infinity,
+        // width: double.infinity,
+        // fit: BoxFit.cover,
+        // ),
+
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            //new Row(
+            // mainAxisSize: MainAxisSize.max,
+
+            //children: <Widget>[
+            new Text(
+              storageFile.key,
+            ),
+            const SizedBox(
+              height: 12,
+            )
+          ],
+        ),
+        // child: Text(storageFile.key),
       ),
     );
   }
